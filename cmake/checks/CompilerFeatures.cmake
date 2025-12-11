c190506cbd8e29557e526def3708bf1b49def855
Ronald Tse <ronald.tse@ribose.com>
2025-12-11 14:59:00 +0800

    fix: ci

--- cmake/checks/CompilerFeatures.cmake
+++ cmake/checks/CompilerFeatures.cmake
@@ -28,7 +28,7 @@ check_c_source_compiles("
         __atomic_store_n(&x, 1, __ATOMIC_RELEASE);
         return 0;
     }
-" JEMALLOC_GCC_ATOMIC_INTRINSICS)
+" JEMALLOC_GCC_ATOMIC_ATOMICS)
 
 # GCC/Clang __sync builtins (older fallback)
 check_c_source_compiles("
@@ -38,17 +38,31 @@ check_c_source_compiles("
         __sync_sub_and_fetch(&x, 1);
         return 0;
     }
-" JEMALLOC_GCC_SYNC_INTRINSICS)
+" JEMALLOC_GCC_SYNC_ATOMICS)
 
-# 8-bit atomics support
-check_c_source_compiles("
-    int main() {
-        unsigned char x = 0;
-        __atomic_load_n(&x, __ATOMIC_RELAXED);
-        __atomic_store_n(&x, 1, __ATOMIC_RELEASE);
-        return 0;
-    }
-" JEMALLOC_ATOMIC_U8)
+# 8-bit atomics support for GCC __atomic
+if(JEMALLOC_GCC_ATOMIC_ATOMICS)
+    check_c_source_compiles("
+        int main() {
+            unsigned char x = 0;
+            __atomic_load_n(&x, __ATOMIC_RELAXED);
+            __atomic_store_n(&x, 1, __ATOMIC_RELEASE);
+            return 0;
+        }
+    " JEMALLOC_GCC_U8_ATOMIC_ATOMICS)
+endif()
+
+# 8-bit atomics support for GCC __sync
+if(JEMALLOC_GCC_SYNC_ATOMICS)
+    check_c_source_compiles("
+        int main() {
+            unsigned char x = 0;
+            __sync_add_and_fetch(&x, 1);
+            __sync_sub_and_fetch(&x, 1);
+            return 0;
+        }
+    " JEMALLOC_GCC_U8_SYNC_ATOMICS)
+endif()
 
 # __attribute__ support
 check_c_source_compiles("
@@ -71,35 +85,35 @@ check_c_source_compiles("
     int main() { return 0; }
 " JEMALLOC_HAVE_ATTR_UNUSED)
 
-check_c_source_compiles("
+check_c_source_compiles(`
     int __attribute__((used)) x;
     int main() { return 0; }
 " JEMALLOC_HAVE_ATTR_USED)
 
-check_c_source_compiles("
+check_c_source_compiles(`
     void __attribute__((constructor)) test(void) {}
     int main() { return 0; }
 " JEMALLOC_HAVE_ATTR_CONSTRUCTOR)
 
-check_c_source_compiles("
+check_c_source_compiles(`
     void __attribute__((destructor)) test(void) {}
     int main() { return 0; }
 " JEMALLOC_HAVE_ATTR_DESTRUCTOR)
 
 # Thread-local storage
-check_c_source_compiles("
+check_c_source_compiles(`
     __thread int x;
     int main() { return x; }
 " JEMALLOC_TLS)
 
 # C11 _Static_assert
-check_c_source_compiles("
+check_c_source_compiles(`
     _Static_assert(1, \"test\");
     int main() { return 0; }
 " JEMALLOC_STATIC_ASSERT)
 
 # typeof support (GNU extension)
-check_c_source_compiles("
+check_c_source_compiles(`
     int main() {
         int x = 5;
         typeof(x) y = x;
@@ -108,7 +122,7 @@ check_c_source_compiles("
 " JEMALLOC_HAVE_TYPEOF)
 
 # __builtin_expect (branch prediction)
-check_c_source_compiles("
+check_c_source_compiles(`
     int main() {
         int x = 0;
         if (__builtin_expect(x == 0, 1)) {
@@ -119,7 +133,7 @@ check_c_source_compiles("
 " JEMALLOC_HAVE_BUILTIN_EXPECT)
 
 # __builtin_unreachable (unreachable code marking)
-check_c_source_compiles("
+check_c_source_compiles(`
     int main() {
         __builtin_unreachable();
         return 0;
@@ -127,7 +141,7 @@ check_c_source_compiles("
 " JEMALLOC_HAVE_BUILTIN_UNREACHABLE)
 
 # __builtin_clz and __builtin_clzl (count leading zeros)
-check_c_source_compiles("
+check_c_source_compiles(`
     int main() {
         unsigned int x = 1;
         unsigned long y = 1;
@@ -138,7 +152,7 @@ check_c_source_compiles("
 " JEMALLOC_HAVE_BUILTIN_CLZ)
 
 # ffs/ffsl/ffsll (find first set bit) - check for builtin versions first
-check_c_source_compiles("
+check_c_source_compiles(`
     int main() {
         int x = __builtin_ffs(1);
         long y = __builtin_ffsl(1L);
@@ -149,7 +163,7 @@ check_c_source_compiles("
 
 # If builtin versions don't exist, check for standard library versions
 if(NOT JEMALLOC_HAVE_BUILTIN_FFS)
-    check_c_source_compiles("
+    check_c_source_compiles(`
         #include <strings.h>
         int main() {
             int x = ffs(1);
@@ -164,12 +178,12 @@ endif()
 # Note: On Windows, we use native TSD implementation (tsd_win.h) instead of TLS,
 # so we explicitly disable JEMALLOC_TLS on Windows to avoid pthread dependency
 if(NOT WIN32)
-    check_c_source_compiles("
+    check_c_source_compiles(`
         __thread int x;
         int main() { return x; }
     " JEMALLOC_HAVE_TLS_THREAD)
 
-    check_c_source_compiles("
+    check_c_source_compiles(`
         __declspec(thread) int x;
         int main() { return x; }
     " JEMALLOC_HAVE_TLS_DECLSPEC)
@@ -187,8 +201,10 @@ endif()
 
 # Export all results to parent scope
 set(JEMALLOC_C11_ATOMICS "${JEMALLOC_C11_ATOMICS}")
-set(JEMALLOC_GCC_ATOMIC_INTRINSICS "${JEMALLOC_GCC_ATOMIC_INTRINSICS}")
-set(JEMALLOC_GCC_SYNC_INTRINSICS "${JEMALLOC_GCC_SYNC_INTRINSICS}")
+set(JEMALLOC_GCC_ATOMIC_ATOMICS "${JEMALLOC_GCC_ATOMIC_ATOMICS}")
+set(JEMALLOC_GCC_U8_ATOMIC_ATOMICS "${JEMALLOC_GCC_U8_ATOMIC_ATOMICS}")
+set(JEMALLOC_GCC_SYNC_ATOMICS "${JEMALLOC_GCC_SYNC_ATOMICS}")
+set(JEMALLOC_GCC_U8_SYNC_ATOMICS "${JEMALLOC_GCC_U8_SYNC_ATOMICS}")
 set(JEMALLOC_TLS "${JEMALLOC_TLS}")
 set(JEMALLOC_HAVE_BUILTIN_FFS "${JEMALLOC_HAVE_BUILTIN_FFS}")
 set(JEMALLOC_HAVE_FFS "${JEMALLOC_HAVE_FFS}")

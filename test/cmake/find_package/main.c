822dac169c66e1a2e84680a20ef11ff25ea7e24c
Ronald Tse <ronald.tse@ribose.com>
2025-12-12 09:07:56 +0800

    feat: vcpkg and cmake support

---
+++ test/cmake/find_package/main.c
@@ -0,0 +1,19 @@
+#include <jemalloc/jemalloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    void* ptr = malloc(1024);
+    if (!ptr) {
+        fprintf(stderr, "malloc failed\n");
+        return 1;
+    }
+
+    printf("jemalloc version: %s\n", JEMALLOC_VERSION);
+    printf("Allocation successful: %p\n", ptr);
+
+    free(ptr);
+    
+    printf("Test passed: find_package(jemalloc) integration works!\n");
+    return 0;
+}
